2d49c455c922e38c463f10d2111234a739eafa06
David Isaksson <davidisaksson93@gmail.com>
2016-04-15 14:18:17 +0200

    Lade till Enemy.h

---
+++ HoppHen/Enemy.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <qrect.h>
+#include "defines.h"
+#include <QPainter>
+
+
+class Enemy
+{
+public:
+	Enemy();
+	Enemy(int xPos, int yPos);
+	~Enemy();
+
+
+	void initialize();
+	void update(float worldSpeed);
+	void paint(QPainter& painter) const;
+
+	//Movement
+	void MoveRight();
+	void MoveLeft();
+	void MoveDampen();
+
+	QRect* getRect() { return enemyRect; }
+
+private:
+	QPixmap* enemyTexture;
+	QRect* enemyRect;
+	float xVel;
+	float moveSpeed;
+	float maxSpeed;
+	float xDampen;
+};
